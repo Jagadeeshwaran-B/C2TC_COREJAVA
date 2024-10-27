feff3888d8870e4c953cfea099cd7391cc710d75
Jagadeeshwaran-B <jagadeeshwaran500@gmail.com>
2024-10-27 20:27:00 +0530

    DAY TWO

---
+++ C2TC_JAGADEESHWARAN/src/com/tns/ifet/daytwo/DoWhileDemo.java
@@ -0,0 +1,12 @@
+package com.tns.ifet.daytwo;
+
+public class DoWhileDemo {
+	public static void main(String[] args) {
+		int i = 17;
+		do {
+		System.out.println(i);
+		i++;
+		} while (i <= 1);
+		}
+
+}
---
+++ C2TC_JAGADEESHWARAN/src/com/tns/ifet/daytwo/IdentifiersDemo.java
@@ -0,0 +1,12 @@
+package com.tns.ifet.daytwo;
+
+public class IdentifiersDemo {
+
+	public static void main(String[] args) {
+		int $number = 50;
+		System.out.println("value of the number variable is : "+ $number);
+		String studentName="Aniket";
+		System.out.println("The student name is : "+ studentName);
+	}
+
+}
