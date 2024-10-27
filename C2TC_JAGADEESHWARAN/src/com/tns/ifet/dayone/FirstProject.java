f97a242ba3d06097191e172ca60c2ecb52712c4f
Jagadeeshwaran-B <jagadeeshwaran500@gmail.com>
2024-10-27 11:18:36 +0530

    Initial Commit

---
+++ C2TC_JAGADEESHWARAN/src/com/tns/ifet/dayone/FirstProject.java
@@ -0,0 +1,10 @@
+package com.tns.ifet.dayone;
+
+public class FirstProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to JAVA!");
+	}
+
+}
