dea023d0d27652960d2ca16486daf35f7e3ecdf6
Jagadeeshwaran-B <jagadeeshwaran500@gmail.com>
2024-11-14 10:02:12 +0530

    Add files via upload

---
+++ C2TC_JAGADEESHWARAN/src/com/tns/ifet/dayeleven/v1/Demo.java
@@ -0,0 +1,11 @@
+//Program without exception handling
+package com.tns.ifet.dayeleven.v1;
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("the program continues....");
+		@SuppressWarnings("unused")
+		int data=100/0;
+	}
+	
+}
